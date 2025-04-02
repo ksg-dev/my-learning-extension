ef6f00e1737ec38192ad4970f7698462aab99d3a
ksg-dev <ksg.dev.data@gmail.com>
2025-04-02 17:59:28 -0500

    working on dom interaction

--- service-worker.js
+++ service-worker.js
@@ -29,7 +29,7 @@ chrome.runtime.onInstalled.addListener(() => {
   });
   
   chrome.runtime.onMessage.addListener((message, sender) => {
-    // The callback for runtime.onMessage must return falsy if we're not sending a response
+    // The callback for runtime.onMessage must return false if we're not sending a response
     (async () => {
       if (message.type === 'open_side_panel') {
         // This will open a tab-specific side panel only on the current tab.
